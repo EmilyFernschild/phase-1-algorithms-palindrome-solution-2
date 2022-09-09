d632b92e675e2175b1a61be2456db34d9ea7b682
Emily Fernschild <emilyfernschild@gmail.com>
2022-09-09 15:03:09 -0400

    commit tree 84445d97abec1631875ff127c88d4559db3afd24

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for(let i = 0; i < word.length / 2; i++){
+    const j = word.length - 1 - i;
+    const startChar = word[i];
+    const endChar = word[j];
+    if(startChar !== endChar) return false;
+  }
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+  racecar   robot
+  ^     ^   ^   -
+   ^   ^    False
+    ^ ^
+     ^
+    True 
 */
 
 /*
-  Add written explanation of your solution here
+  i is ir=terating through the word by each letter starting with the first letter 
+  whereas j is iterating through the word starting with the last letter
+  if the starting character does not equal the last character and so on then the word is no a palindrom
+  if they do match all the way through till the middle then the word is a palindrom
 */
 
 // You can run `node index.js` to view these console logs
